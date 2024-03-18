869698055f005f614bd044cc9db6d54a3b93f8d7
Rabbitou <48840164+Rabbitou@users.noreply.github.com>
2024-03-18 00:40:57 +0000

    feat: fix quote api

--- src/app/component/motivational-quotes/motivational-quotes.component.ts
+++ src/app/component/motivational-quotes/motivational-quotes.component.ts
@@ -1,25 +1,26 @@
-import { Component } from '@angular/core';
-import { MotivationalQuotesService } from 'src/app/services/motivational-quotes.service';
+import { Component } from "@angular/core";
+import { MotivationalQuotesService } from "src/app/services/motivational-quotes.service";
 
 @Component({
-  selector: 'mg-motivational-quotes',
-  templateUrl: './motivational-quotes.component.html',
-  styleUrls: ['./motivational-quotes.component.scss']
+  selector: "mg-motivational-quotes",
+  templateUrl: "./motivational-quotes.component.html",
+  styleUrls: ["./motivational-quotes.component.scss"],
 })
 export class MotivationalQuotesComponent {
   constructor(private motivationalQuoteService: MotivationalQuotesService) {}
+  quoteP: string = "Loading...";
 
-  // Define a method to fetch a motivational quote
+  ngOnInit() {
+    this.fetchMotivationalQuote();
+  }
+
   fetchMotivationalQuote() {
-    this.motivationalQuoteService.getMotivationalQuote().subscribe(
-      quote => {
-        // Handle the received quote data here
-        console.log(quote);
+    this.motivationalQuoteService.getMotivationalQuote().subscribe({
+      next: (quote: any) => (this.quoteP = quote[0].q),
+      error: (error) => {
+        this.quoteP = "Something went wrong check console !";
+        console.log(error);
       },
-      error => {
-        // Handle error
-        console.error(error);
-      }
-    );
+    });
   }
 }
--- src/app/services/motivational-quotes.service.ts
+++ src/app/services/motivational-quotes.service.ts
@@ -1,15 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MotivationalQuotesService {
+  private zenQuotesUrl = "https://proxy-js-quotes.vercel.app/api";
 
-  private zenQuotesUrl = 'https://zenquotes.io/api/random';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMotivationalQuote(): Observable<any> {
     return this.http.get<any>(this.zenQuotesUrl);
